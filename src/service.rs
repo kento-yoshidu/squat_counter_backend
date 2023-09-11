1f72c48c08fdd047874032f30166f2c25a102473
kento <kento@toriwatari.bird>
2023-09-11 21:37:48 +0900

    ⚡ Update

--- src/service.rs
+++ src/service.rs
@@ -1,8 +1,14 @@
+use dotenv::dotenv;
+use std::env;
+
+use aws_sdk_dynamodb::{Client};
+
 #[allow(unused)]
 use actix_web::{
     get, post, web,
     web::{Data, Json, Path},
-    Responder, HttpResponse
+    Responder, HttpResponse,
+    Result
 };
 
 #[allow(unused)]
@@ -15,9 +21,40 @@ struct User {
     name: String,
 }
 
+#[derive(Debug, Serialize)]
+pub enum ApiErrorResponse {
+    AuthenticationFailure,
+    NeedsAuthentication,
+}
+
+#[derive(Debug, Serialize)]
+pub struct ApiResponseBody {
+    pub success: bool,
+    pub errcode: u16,
+    pub message: String,
+}
+
 #[get("/test")]
 pub async fn test() -> impl Responder {
     let user = User { id: 1, name: String::from("test_user") };
 
     web::Json(user)
+}
+
+#[get("/fetch")]
+pub async fn fetch() -> Result<impl Responder> {
+    let config = aws_config::load_from_env().await;
+    let client = Client::new(&config);
+
+    dotenv().ok();
+
+    let table_name = env::var("TABLE_NAME").unwrap();
+
+    let resp = client.scan().table_name(table_name).send().await;
+
+    println!("response = {:?}", resp);
+
+    let user = User { id: 1, name: String::from("hoge") };
+
+    Ok(web::Json(user))
 }
