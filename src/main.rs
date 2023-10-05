5e82bbb8b7834886ecb6ab57ac7ba538986819fe
kento <kento@toriwatari.bird>
2023-10-06 07:05:25 +0900

    ⚡ Setting Debug Log

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,13 @@ use actix_cors::Cors;
 mod service;
 use service::{fetch_service, add_service, update_service};
 
+use env_logger::Env;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    env_logger::init_from_env(Env::default().default_filter_or("debug"));
+
     HttpServer::new(|| {
         App::new()
             .wrap(
@@ -16,8 +21,7 @@ async fn main() -> std::io::Result<()> {
                     .allowed_origin("http://localhost:3000")
                     .allowed_methods(vec!["GET", "POST"])
                     .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
-                    // .allowed_header(header::CONTENT_TYPE)
-                    .allowed_header(http::header::CONTENT_TYPE)
+                    .allowed_header(header::CONTENT_TYPE)
                     .supports_credentials()
                     .max_age(3600),
             )
